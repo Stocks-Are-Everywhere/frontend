c4b4f33e86318cdad8084154e7eb34e31322478e
Hugh <lee2317884@gmail.com>
2025-02-18 20:04:05 +0900

    feat: add user section to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,11 +11,21 @@ const Header: React.FC = () => {
         </LogoSection>
 
         <NavSection>
-          <NavItem active>홈</NavItem>
-          <NavItem>주식</NavItem>
+          <NavItem>홈</NavItem>
+          <NavItem active>주식</NavItem>
           <NavItem>투자내역</NavItem>
           <NavItem>자산</NavItem>
         </NavSection>
+
+        <UserSection>
+          <Balance>
+            <BalanceLabel>투자자산</BalanceLabel>
+            <BalanceAmount>1,000,000원</BalanceAmount>
+          </Balance>
+          <UserProfile>
+            <ProfileImage src="/images/default-profile.png" />
+          </UserProfile>
+        </UserSection>
       </HeaderContent>
     </HeaderContainer>
   );
@@ -71,6 +81,42 @@ const NavItem = styled.a<{ active?: boolean }>`
   &:hover {
     color: #333d4b;
   }
+`;
+
+const UserSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+`;
+
+const Balance = styled.div`
+  text-align: right;
+`;
+
+const BalanceLabel = styled.div`
+  font-size: 12px;
+  color: #8b95a1;
+  margin-bottom: 4px;
+`;
+
+const BalanceAmount = styled.div`
+  font-size: 16px;
+  font-weight: 700;
+  color: #333d4b;
+`;
+
+const UserProfile = styled.div`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: #f2f2f2;
+`;
+
+const ProfileImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
 export default Header;
