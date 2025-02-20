c849c9100036111973b4f37d3b320d851041b479
Hugh <lee2317884@gmail.com>
2025-02-20 16:05:03 +0900

    feat: implementing a Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,27 @@
 import React from 'react';
-
-import TradeHistoryList from './components/TradeHistory';
-import OrderBook from './components/OrderBook';
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/GlobalStyle';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import styled from 'styled-components';
-import StockChart from './components/StockChart';
+import TradingPage from './pages/TradingPage';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <AppContainer>
-        <Header />
-        <MainContent>
-          <OrderBook />
-          <TradeHistoryList />
-          <StockChart />
-        </MainContent>
-        <Footer />
-      </AppContainer>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Main>
+        <TradingPage />
+      </Main>
+      <Footer />
+    </>
   );
 };
 
-const AppContainer = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-`;
-
-const MainContent = styled.main`
+const Main = styled.main`
   flex: 1;
-  margin-top: 64px; // 헤더 높이만큼 여백
-  padding: 24px;
+  width: 100%;
+  background-color: #f5f6f7;
 `;
 
 export default App;
